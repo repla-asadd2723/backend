ddce5604f226fa804eb243bd735f4b2e482b5c7b
asadd2723 <asadu3040@gmail.com>
2025-02-16 03:19:16 +0500

    push newchanging

--- db/connect.js
+++ db/connect.js
@@ -4,17 +4,12 @@ const mongoose = require('mongoose');
 
 const uri = "mongodb+srv://pkprogrammer1:MNMdMqm5VJNXPNWf@cluster-backend.vaizo.mongodb.net/cluster-backend?retryWrites=true&w=majority&appName=cluster-backend"
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(uri, {
+const connectDB = () => {
+    return mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  }
-  catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
 }
+
+module.exports = connectDB;
