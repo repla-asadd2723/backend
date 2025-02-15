8a0b88e717b6768db3b3a6de51eaa8bcecdf3930
asadd2723 <asadu3040@gmail.com>
2025-02-16 02:53:37 +0500

    add db url

--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,20 @@
 const db = "mongodb://localhost:27017/mean";
+
+const mongoose = require('mongoose');
+
+const uri = "mongodb+srv://pkprogrammer1:MNMdMqm5VJNXPNWf@cluster-backend.vaizo.mongodb.net/cluster-backend?retryWrites=true&w=majority&appName=cluster-backend"
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  }
+  catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
