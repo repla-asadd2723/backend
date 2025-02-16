558d9ef553b5ef2102d130df387c71bb17b9eb5c
asadd2723 <asadu3040@gmail.com>
2025-02-16 22:21:16 +0500

    push login register and profile api

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://king-prawn-app-y5pls.ondigitalocean.app" }));
+
 
 app.get('/', (req, res) => {
   res.send('Hello World');
