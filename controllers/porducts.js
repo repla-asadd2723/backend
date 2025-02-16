48e379247354a887f8d042bb2599670ef72aef1d
asadd2723 <asadu3040@gmail.com>
2025-02-16 20:02:01 +0500

    push

--- controllers/porducts.js
+++ controllers/porducts.js
@@ -1,7 +1,7 @@
 const products = require('../models/products');
 
 const getAllProducts = (req, res) => {
-  products.find({})
+  products.find({"featured": true, "type": "Electronics"})
     .then(data => res.status(200).json({ success: true, data }))
     .catch(err => res.status(500).json({ success: false, msg: 'Internal server error' }));
 };
